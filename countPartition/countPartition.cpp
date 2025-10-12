6b7ea16c7630fc19298b540039a40224e893e2b1
ArponMazumdar <arponmazumdar96@gmail.com>
2025-10-12 16:08:42 +0600

    commit

--- countPartition/countPartition.cpp
+++ countPartition/countPartition.cpp
@@ -18,10 +18,27 @@ int count_Partition(int n, std::vector<std :: vector <int>> &dp, int k = 1) {
     return dp[n][k] = count;
 }
 
+int cnt_Partition(int &num){
+    std :: vector <std :: vector <int>> dp(num + 1, std :: vector <int>(num + 1, 0));
+
+    for(int i = 0; i <= num; i++){
+        dp[i][0] = 1;
+    }
+
+    for(int i = 1; i <= num; i++){
+        int cnt = 0;
+        for(int j = i; j >= 1; j--){
+            cnt += dp[i - j][j];
+        }
+        dp[i][i] = cnt;
+    }
+    return dp[num + 1][num + 1];
+}
+
 int main() {
     int n = 5;
     std::vector<std :: vector <int>> dp(n + 1, std :: vector <int>(n + 1, -1));
-    int ans = count_Partition(n, dp);
+    int ans = cnt_Partition(n);
     std::cout << "Partition for " << n << " = " << ans << std::endl;
     return 0;
 }
--- countSubsetSumToK/count.cpp
+++ countSubsetSumToK/count.cpp
@@ -24,11 +24,33 @@ int count(std :: vector <int> &nums, std :: vector <std :: vector <int>> &dp, in
     return dp[i][sum] = counter;
 }
 
+int cnt(int &n, std :: vector <int> &nums){
+    int len = nums.size();
+    std :: vector <std :: vector <int>> dp(len, std :: vector <int> (n + 1, 0));
+
+    for(int i = 0; i < len; i++){
+        dp[i][0] = 1;
+    }
+
+    for(int i = len - 1; i >= 0; i--){
+        for(int j = 1; j <= n; j++){
+            dp[i][j] += (j >= nums[i]) ? dp[i][j - nums[i]] : 0;
+            dp[i][j] += (i + 1 < len) ? dp[i + 1][j] : 0; 
+        }
+    }
+
+    return dp[0][n];
+}
+
 int main(){
-    std :: vector <int> nums = {1, 2, 3, 4};
-    int sum = 4;
+    std :: vector <int> nums = {1, 2, 4, 5, 8, 11, 9};
+    int sum = 17;
     int n = nums.size();
     std :: vector <std :: vector <int>> dp(n, std :: vector <int>(sum + 1, -1));
     int ans = count(nums, dp, n, sum);
-    std :: cout << ans << std :: endl;
+    int ans_1 = cnt(sum, nums);
+    std :: cout << ans << std :: endl
+    << ans_1 << std :: endl;
+
+    return 0;
 }
