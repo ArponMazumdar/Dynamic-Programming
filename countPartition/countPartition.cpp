a97fedd691b6579e9ce3e859e421bb8ffc022d8b
ArponMazumdar <arponmazumdar96@gmail.com>
2025-10-12 16:20:52 +0600

    modified

--- countPartition/countPartition.cpp
+++ countPartition/countPartition.cpp
@@ -25,20 +25,23 @@ int cnt_Partition(int &num){
         dp[i][0] = 1;
     }
 
-    for(int i = 1; i <= num; i++){
-        int cnt = 0;
-        for(int j = i; j >= 1; j--){
-            cnt += dp[i - j][j];
+    for(int i = num; i >= 0; i--){
+        for(int j = 1; j <= num; j++){
+            dp[i][j] += (j >= i) ? dp[i][j - i] : 0;
+            dp[i][j] += (i < num) ? dp[i + 1][j] : 0;
         }
-        dp[i][i] = cnt;
     }
-    return dp[num + 1][num + 1];
+    return dp[0][num];
 }
 
 int main() {
-    int n = 5;
+    int n = 14;
     std::vector<std :: vector <int>> dp(n + 1, std :: vector <int>(n + 1, -1));
-    int ans = cnt_Partition(n);
-    std::cout << "Partition for " << n << " = " << ans << std::endl;
+    int ans = count_Partition(n, dp);
+    int ans_1 = cnt_Partition(n);
+    
+    std::cout << "Partition for " << n << " : \n"
+    << ans << std :: endl
+    << ans_1 << std :: endl;
     return 0;
 }
