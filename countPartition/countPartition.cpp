d01f1bc5879add32884e794b2e4a41d2d7791927
ArponMazumdar <arponmazumdar96@gmail.com>
2025-10-12 23:06:12 +0600

    edited

--- countPartition/countPartition.cpp
+++ countPartition/countPartition.cpp
@@ -17,6 +17,25 @@ int count_Partition(int n, std::vector<std :: vector <int>> &dp, int k = 1) {
     return dp[n][k] = count;
 }
 
+int count_Partitions(int &num){
+    std :: vector <std :: vector <int>> dp(num + 1, std :: vector <int>(num + 1, 0));
+
+    for(int i = 1; i <= num; i++){
+        dp[i][0] = 1;
+    }
+
+    for(int i = num; i >= 1; i--){
+        for(int j = 1; j <= num; j++){
+            int count = 0;
+            for(int k = i; k <= num; k++){
+                count += (j >= k) ? dp[k][j - k] : 0;
+            }
+            dp[i][j] = count;
+        }
+    }
+    return dp[1][num];
+}
+
 int cnt_Partition(int &num){
     std :: vector <std :: vector <int>> dp(num + 1, std :: vector <int>(num + 1, 0));
 
@@ -24,13 +43,13 @@ int cnt_Partition(int &num){
         dp[i][0] = 1;
     }
 
-    for(int i = num; i >= 0; i--){
+    for(int i = num; i >= 1; i--){
         for(int j = 1; j <= num; j++){
             dp[i][j] += (j >= i) ? dp[i][j - i] : 0;
             dp[i][j] += (i < num) ? dp[i + 1][j] : 0;
         }
     }
-    return dp[0][num];
+    return dp[1][num];
 }
 
 int main() {
@@ -38,9 +57,11 @@ int main() {
     std::vector<std :: vector <int>> dp(n + 1, std :: vector <int>(n + 1, -1));
     int ans = count_Partition(n, dp);
     int ans_1 = cnt_Partition(n);
+    int ans_2 = count_Partitions(n);
 
     std::cout << "Partition for " << n << " : \n"
     << ans << std :: endl
-    << ans_1 << std :: endl;
+    << ans_1 << std :: endl
+    << ans_2 << std :: endl;
     return 0;
 }
